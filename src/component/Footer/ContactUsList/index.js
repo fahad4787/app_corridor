efa60cc9a110b29c92980c132085c15d5e7b12ee
fahad4787 <nfahad4787@gmail.com>
2023-01-10 12:27:58 +0500

    12:27pm/01-10-2k23

--- src/component/Footer/ContactUsList/index.js
+++ src/component/Footer/ContactUsList/index.js
@@ -8,7 +8,7 @@ const ContactUsList = () => {
   const [data] = useState(ContactUsListData);
 
   return (
-    <Col md={4} className="d-sm-flex justify-content-center">
+    <Col md={4} className="d-sm-flex justify-content-md-start justify-content-center">
       <ul className={classes.contact_us_list}>
         {data.map((currn, ind) => {
           return <ContactListLink key={ind} {...currn} />;
